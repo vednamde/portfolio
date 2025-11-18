d0d48251852d89a1d02de96fbdcaa2fb6fcf5b40
Ved Namde <vednamde@gmail.com>
2025-11-18 14:10:31 +0530

    Add LiquidEther background to all sections

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,21 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import LiquidEther from "./components/LiquidEther";
 
 export default function App() {
   return (
-    <div className="bg-[#05060B] text-white">
+    <div className="bg-[#05060B] text-white" style={{ position: 'relative', minHeight: '100vh' }}>
+      <LiquidEther
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          zIndex: -1
+        }}
+      />
       <Navbar />
       <Hero />
       <About />
